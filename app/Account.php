2944bd82e0180471e4ff6365936556eea0b72a11
Cody Finegan <temporalslide@users.noreply.github.com>
2015-10-03 19:51:38 +1300

    Adding the Account

--- app/Account.php
+++ app/Account.php
@@ -2,6 +2,22 @@
 
 namespace Tidy;
 
-class Account
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Mode
 {
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'accounts';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'address', 'description'];
+
 }
