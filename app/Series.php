b3ddbe1630f650adb42f85ddfa32bdf1706b8f5a
Cody Finegan <temporalslide@users.noreply.github.com>
2015-10-28 22:07:49 +1300

    Adding scopes to the series

--- app/Series.php
+++ app/Series.php
@@ -7,6 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class Series extends Model
 {
     protected $table = 'series';
-    
+
     protected $fillable = ['title', 'description', 'for_dvd', 'for_bluray', 'for_book'];
+
+    public function scopeForDvd($query)
+    {
+        return $query->where('for_dvd', true);
+    }
+
+    public function scopeForBluray($query)
+    {
+        return $query->where('for_bluray', true);
+    }
+
+    public function scopeForBook($query)
+    {
+        return $query->where('for_book', true);
+    }
 }
